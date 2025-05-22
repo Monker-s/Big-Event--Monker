274389ee2bffaa4f8a9858fbf9f34a6fbb84a83e
Monker-s <2685986106@qq.com>
2025-05-22 21:38:22 +0800

    element-plus的组件按需导入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-
+//在Vue3 Composition API中 
+//1.获取路由对象router  const router = useRouter()
+//2.获取当前路由参数 const route = useRoute()
 
 
 
@@ -8,6 +10,7 @@
 <template>
  <div>
   我是APP
+  <el-button>按钮</el-button>
  </div>
 </template>
 
